bee5a2103282b07150b09867a8c4a0dac98088e9
Aleksa Maksimovic <maksimovic1919@gmail.com>
2022-08-16 17:07:13 +0200

    init

---
+++ src/main/java/rs/gov/mup/networkintrusiondetection/NetworkIntrusionDetectionApplication.java
@@ -0,0 +1,13 @@
+package rs.gov.mup.networkintrusiondetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkIntrusionDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkIntrusionDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/gov/mup/networkintrusiondetection/NetworkIntrusionDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.gov.mup.networkintrusiondetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkIntrusionDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
